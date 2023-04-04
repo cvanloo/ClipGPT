85baf5937a9cc2440e89b2e34b575aa1eea2583c
Colin van Loo <colin.vanloo@ch.zetes.com>
2023-04-04 11:43:11 +0200

    fix(cfg): closing form bypasses validation
    Closing the form does not validate the settings (which is correct so).
    However, since the `savedBehaviours` is directly bound, it could end up
    with an invalid value.
    We now copy the current settings into a new list, and copy back from
    this list only on save.

--- ClipGpt7/SettingsForm.cs
+++ ClipGpt7/SettingsForm.cs
@@ -6,7 +6,9 @@ namespace ClipGpt7;
 public partial class SettingsForm : Form
 {
 	private readonly IUserSettings _userSettings;
-	private IBindingList? _chatBehaviourList;
+	
+	private List<string>? _chatBehaviours;
+	private IBindingList? _chatBehavioursData;
 
 	public SettingsForm(IUserSettings userSettings)
 	{
@@ -18,19 +20,19 @@ public partial class SettingsForm : Form
 
 	private void buttonClose_Click(object sender, EventArgs e)
 	{
-		if (comboBoxChatBehaviour.SelectedIndex < 0)
+		if (_chatBehaviours == null || comboBoxChatBehaviour.SelectedIndex < 0)
 		{
 			MessageBox.Show("Select a Chat Behaviour.", "Validation Error", MessageBoxButtons.OK,
 				MessageBoxIcon.Warning);
 			return;
 		}
-		
+
 		_userSettings.Temperature = trackBarTemperature.Value;
 		_userSettings.MaxTokens = (int)numericUpDownMaxTokens.Value;
 		_userSettings.ApiKey = textBoxApiKey.Text;
 		_userSettings.LanguageModel = ((ModelTypeCombo)comboBoxLanguageModel.SelectedItem).Id;
 		_userSettings.CompletionMethod = ((CompletionTypeCombo)comboBoxCompletionMethod.SelectedItem).Id;
-		// _userSettings.SavedBehaviours is directly bound to the DataSource and therefore updates automatically.
+		_userSettings.SavedBehaviours = _chatBehaviours;
 		_userSettings.SelectedBehaviour = comboBoxChatBehaviour.SelectedIndex;
 		_userSettings.Save();
 		Close();
@@ -56,8 +58,9 @@ public partial class SettingsForm : Form
 		comboBoxLanguageModel.DataSource = languageModelSource;
 		comboBoxLanguageModel.SelectedItem = languageModelSource.First(lm => lm.Id == _userSettings.LanguageModel);
 
-		_chatBehaviourList = new BindingList<string>(_userSettings.SavedBehaviours);
-		comboBoxChatBehaviour.DataSource = _chatBehaviourList;
+		_chatBehaviours = new List<string>(_userSettings.SavedBehaviours);
+		_chatBehavioursData = new BindingList<string>(_chatBehaviours);
+		comboBoxChatBehaviour.DataSource = _chatBehavioursData;
 		comboBoxChatBehaviour.SelectedIndex = _userSettings.SelectedBehaviour;
 	}
 
@@ -70,22 +73,22 @@ public partial class SettingsForm : Form
 
 	private void comboBoxChatBehaviour_KeyDown(object sender, KeyEventArgs e)
 	{
-		if (_chatBehaviourList == null) return;
-		
+		if (_chatBehavioursData == null) return;
+
 		switch (e.KeyCode)
 		{
 			case Keys.Enter:
 				var addLine = comboBoxChatBehaviour.Text;
 				if (!string.IsNullOrWhiteSpace(addLine))
 				{
-					_chatBehaviourList.Add(addLine);
-					comboBoxChatBehaviour.SelectedIndex = _chatBehaviourList.Count - 1;
+					_chatBehavioursData.Add(addLine);
+					comboBoxChatBehaviour.SelectedIndex = _chatBehavioursData.Count - 1;
 				}
 				break;
 			case Keys.Delete:
 				if (comboBoxChatBehaviour.SelectedIndex > -1)
 				{
-					_chatBehaviourList.RemoveAt(comboBoxChatBehaviour.SelectedIndex);
+					_chatBehavioursData.RemoveAt(comboBoxChatBehaviour.SelectedIndex);
 				}
 				break;
 		}
